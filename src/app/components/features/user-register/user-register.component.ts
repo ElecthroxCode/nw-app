8f6a37f96389af5f8694302aa30b398699d9423e
ElecthroxCode <ehx.software@gmail.com>
2025-05-19 00:29:11 -0500

    refactor(user-register): move template from HTML file to inline template

--- src/app/components/features/user-register/user-register.component.ts
+++ src/app/components/features/user-register/user-register.component.ts
@@ -3,7 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-register',
   imports: [],
-  templateUrl: './user-register.component.html',
+  template: `
+  <section >
+    
+  <div class="container-register">
+      <div class="zone-msg">
+          <div class="msg-register">
+              <h1>Nuestros clientes son nuestro mayor orgullo</h1>
+              <div>
+                  <p>Crea tu cuenta de forma rápida y fácil</p>
+              </div>
+          
+          </div>
+      </div>
+      
+      <div class="register">
+          
+          <form action="">
+              <h3>Registro de cliente</h3>
+              <div><input type="text" name="" id="" placeholder="Escribe tu nombre y apellido"></div>
+              <div><input type="text" name="" id="" placeholder="Escribe tu email"></div>
+              <div><input type="text" name="" id="" placeholder="Escribe tu télefono"></div>
+              <div><input type="password" name="" id="" placeholder="Escribe tu contraseña"></div>
+              <div><input type="password" name="" id="" placeholder="Repite tu contraseña"></div>
+              <div><button class="btn btn-register">Register</button></div>
+          </form>
+      </div>
+
+  </div>
+</section>
+
+  `,
   styleUrl: './user-register.component.css'
 })
 export class UserRegisterComponent {
